50e4f5506806d87556f7f07411cca098713a4e11
Shakeb Ahmed Khan <Shakeb.Khan@qorenext.com>
2026-05-04 08:54:13 +0530

    Limiter upto 50 per hour

--- extensions/limiter.py
+++ extensions/limiter.py
@@ -9,5 +9,5 @@ from flask_limiter.util import get_remote_address
 
 limiter = Limiter(
     key_func=get_remote_address,
-    default_limits=["200 per day", "100 per hour"],
+    default_limits=["200 per day", "50 per hour"],
 )
